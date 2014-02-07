d2ef9d873920538cce48a161d5dd823f7f2c29b8
Pete Haughie <speedwolf@gmail.com>
2014-02-07 16:02:48 +0000

    Longform article and js

--- article.tpl
+++ article.tpl
@@ -6,40 +6,89 @@
 
     {{ include file="_tpl/header.tpl" }}
 
-    <div id="content" class="clearfix">
+    <div id="content" class="clearfix {{ if $gimme->article->longform }}longform{{ /if }}">
 
       <section class="main entry">
 
-      {{ if $gimme->article->type_name == "debate" }}
+      {{ if $gimme->article->longform }}
+        {{ list_related_articles }}
+        <section id="component-{{ $gimme->article->number }}">
+          <article>
+            {{ if $gimme->article->content_accessible }}
+              <h2 id="{{ $gimme->article->number }}">{{ $gimme->article->name }}</h2>
+              {{ include file="_tpl/img/img_big.tpl" where="article" }}
+              <div class="intro">{{ $gimme->article->deck }}</div>
+              {{ include file="_tpl/_edit-article.tpl" }}{{ $gimme->article->full_text }}
+            {{ else }}
+              <p><em>{{ #thisArticleIsLocked# }}</em></p>
+            {{ /if }}
+          
+            {{ include file="_tpl/article-slideshow.tpl" }}
 
-        {{ include file="_tpl/article-debate.tpl" }}
+            {{ if $gimme->article->has_attachments }}
+              {{ list_article_attachments }}
+                {{ if $gimme->attachment->extension == oga }}
+
+                  <div class="audio-attachment aside-box">
+                    <h2>{{ #listenAudio# }}</h2>
+                    <audio src="{{ uri options="articleattachment" }}" width="290" controls>
+                      <a href="{{ uri options="articleattachment" }}">{{ #downloadAudioFile# }}</a>
+                    </audio>
+                  </div><!-- /.audio-attachment -->
+
+                {{ elseif $gimme->attachment->extension == ogv || $gimme->attachment->extension == ogg || $gimme->attachment->extension == mp4 || $gimme->attachment->extension == webm }}             
+
+                  <div class="video-attachment aside-box"><!-- read http://diveintohtml5.org/video.html -->
+                    <h2>{{ #watchVideo# }}</h2>
+                    <video id="video_{{ $gimme->current_list->index }}" class="video-js vjs-default-skin" controls
+                      preload="auto" width="100%" data-setup="{}">
+                      <source src="{{ uri options="articleattachment" }}" type='{{ $gimme->attachment->mime_type }}'>
+                      <a href="{{ uri options="articleattachment" }}">{{ #download# }} .{{ $gimme->attachment->extension }} {{ #file# }}</a>
+                    </video>
+                  </div><!-- /#video-attachment --> 
+
+                {{ else }}
+
+                  <div class="attachment aside-box">
+                    <h2>{{ #downloadFile# }}</h2>
+                    <p>{{ #fileOfType# }} {{ $gimme->attachment->mime_type }}</p>
+                    <a href="{{ uri options="articleattachment" }}">{{ $gimme->attachment->file_name }} ({{ $gimme->attachment->size_kb }}kb)</a>
+                    <p><em>{{ $gimme->attachment->description }}</em></p>
+                  </div><!-- /.attachment -->
+
+                {{ /if }}      
+              {{ /list_article_attachments }}
+            {{ /if }} 
+
+          </article>
+        </section>
+        {{ /list_related_articles }}
 
       {{ else }}
 
-        {{ include file="_tpl/article-cont.tpl" }}
+        {{ if $gimme->article->type_name == "debate" }}
 
-      {{ /if }}
+          {{ include file="_tpl/article-debate.tpl" }}
 
-      {{ if $gimme->article->type_name !== "page" }}
+        {{ else }}
 
-        {{ if $gimme->article->longform }}
-          <div id="longform-article-tools">
-        {{ /if }}
-
-        {{ if $gimme->article->type_name !== "debate" }}
-
-          {{ include file="_tpl/article-author-info.tpl" }}
+          {{ include file="_tpl/article-cont.tpl" }}
 
         {{ /if }}
 
-        {{ include file="_tpl/article-rating.tpl" }}
+        {{ if $gimme->article->type_name !== "page" }}
 
-        {{ include file="_tpl/article-comments.tpl" }}
-        
-        {{ if $gimme->article->longform }}
-          </div>
+          {{ if $gimme->article->type_name !== "debate" }}
+
+            {{ include file="_tpl/article-author-info.tpl" }}
+
+          {{ /if }}
+
+          {{ include file="_tpl/article-rating.tpl" }}
+
+          {{ include file="_tpl/article-comments.tpl" }}
+          
         {{ /if }}
-
       {{ /if }}
 
       </section><!-- / Entry -->
--- assets/js/longform.js
+++ assets/js/longform.js
@@ -1,174 +1,73 @@
 $(document).ready(function(){
+  // these vars get used a whole lot
   var winHeight = $(window).height();
   var winWidth = $(window).width();
-  $('#titles, .thumb-gallery > div figure, .video-attachment').height(winHeight);
-  $('.video-attachment video').width(winWidth);
-  $('#titles h2, #titles .date').css({
-    'line-height': (winHeight - 140) + 'px'
-  });
 
-  // set up the main image for the background
-  var leadImg = $('#leadImg');
-  leadImg.remove();
-  leadImg = leadImg.find('img');
-  var leadImgSrc = leadImg.attr('src');
-  $('body').attr('style', 'background-image: url(' + leadImgSrc + ');');
-
-  // change the videos in the body to native objects
-  $('#cont video').each(function(){
-    $(this).find('object').remove();
-  });
-
-  // set up the galleries
+  // make a mini menu for easier navigation
+  var miniMenu = "<ul id='miniMenu'>";
+  var elemType;
   var i = 1;
-  $('.thumb-gallery').each(function(){
-    i = $(this).parent().find('figure').length;
-    $(this).find('figure').each(function(){
-      var img = $(this).find('img');
-      var imgSrc = img.attr('src');
-      img.hide();
-      $(this).attr('style', 'z-index:' + i + '; background-image: url(' + imgSrc + ');');
-      i = i - 1;
-      $(this).height(winHeight);
-    });
-    i = 1;
+  $('#content h2, #content h3').each(function(){
+    elemType = this.tagName.toLowerCase();
+    elemParent = $(this).parent().parent().attr('id');
+    $(this).addClass(elemType).attr('id', i);
+    miniMenu = miniMenu + "<li class='" + elemType + " " + elemParent + "'><span>+</span><a href='#" + i + "'>" + $(this).text() + "</a></li>";
+    i = i + 1;
   });
-
-  // all this has to happen after the content has loaded or the offsets are incorrect
+  miniMenu = miniMenu + "</ul>";
+  $('body').append(miniMenu);
   setTimeout(function(){
-    // get some offset positions for use in scrolling
-    // $('.gall-box, video').each(function(){
-    //   var offSetY;
-    //   offsetY = $(this).position().top;
-    //   $(this).addClass('skipTo');
-    // });
-    // make a mini menu from the header elements in the document at root level
-    var miniMenu = "<ul id='miniMenu'>"
-    miniMenu = miniMenu + "<li class='h2'><a href='#top'>Title</a></li>";
-    $('#cont > h2, #cont h3, #cont h4').each(function(){
-      var offsetY = $(this).position().top;
-      var type;
-      $(this).attr('id', offsetY);
-      if ($(this).is("h2")) {
-        type = "h2";
-      }
-      if ($(this).is("h3")) {
-        type = "h3";
-      }
-      if ($(this).is("h4")) {
-        type = "h4";
-      }
-      miniMenu = miniMenu + "<li class='" + type + "'><a href='#" + offsetY + "'>" + $(this).html() + "</a></li>"
-    });
-    // is there a gallery? if so, add it
-    if ($('.thumb-gallery')[0]){
-      var gallPos = $('.thumb-gallery').position().top;
-      $('.thumb-gallery').attr('id', gallPos);
-      miniMenu = miniMenu + "<li class='h2'><a href='" + gallPos + "'>Gallery</a></li>";
-    }
-    // are there videos if so, add them
-    // if ($('.video-attachment')[0]){
-    //   var vidContPos = $('.video-attachment').position().top;
-    //   miniMenu = miniMenu + "<li class='h2'><a href='" + vidContPos + "'>Videos</a></li>";
-    //   $('.video-attachment video').each(function(){
-    //     var vidPos = ($(this).attr('id', $(this).position().top));
-    //     miniMenu = miniMenu + "<li class='h2'><a href='" + vidPos + "'>" + $(this).attr('src') + "</a></li>";
-    //   });
-    // }
-    miniMenu = miniMenu + "</ul>"
-    $('#top').append(miniMenu);
-    $('#miniMenu').fadeIn(1000);
-
-    setTimeout(function(){
-      var currOffset;
-      $('#miniMenu').find('a').bind('click', function(){
-        currOffset = $(this).attr('href').replace('#','');
-        if (currOffset == 'top') {
-          currOffset = '0';
+    $('body #miniMenu').fadeIn(1000, function(){
+      // do click events on the miniMenu
+      $('#miniMenu li span').bind('click', function(){
+        var elemHeight = $(this).parent().height();
+        var matchClass = $(this).parent().attr('class');
+        matchClass = matchClass.replace('h2 ','');
+        if ($(this).hasClass('active')){
+          $(this).removeClass('active');
+          $(this).parent().siblings('.' + matchClass).css('height','0');
+        } else {
+          $(this).addClass('active');
+          $(this).parent().siblings('.' + matchClass).css('height', elemHeight);
         }
-        parseInt(currOffset);
+      });
+      $('#miniMenu a').bind('click', function(){
+        var elemMatch = $(this).attr('href');
+        var elemPos = $('#content').find(elemMatch).position().top;
         $('body').animate({
-          scrollTop: currOffset
-        });
-        currOffset = 0;
+          scrollTop: elemPos
+        }, 1000);
+        // console.log(elemPos);
         return false;
       });
-    }, 500);
-  }, 1000);
-
-  // make magic happen on position of viewport
-  setInterval(function(){
-    var currViewportPos = window.pageYOffset;
-    // not working yet
-    // $('.skipTo').each(function(){
-    //   if ($(this).position().top < currViewportPos) {
-    //     scrollTop: $(this).position().top
-    //   }
-    // });
-    $('.video-attachment video').each(function(){
-      // you want this play when the parent container comes into view, not the video as that's been offset
-      var vidPos = $(this).parent().position().top;
-      var vidName = $(this).attr('id');
-      if (currViewportPos > (vidPos - 10)) {
-        $(this)[0].play();
-      } else {
-        $(this)[0].pause();
-      }
-      if (currViewportPos > (vidPos + winHeight)){
-        $(this)[0].pause();
-      }
     });
-  }, 500);
+  }, 3000);
 
-  // keep full screen videos at 0 0 of viewport
-  // has to be outside the previous loop as the tick is much quicker
-  setInterval(function(){
-    var currViewportPos = window.pageYOffset;
-    var vidPos = $('.video-attachment').position().top;
-    var vidOffsetY = currViewportPos - vidPos;
-    $('.video-attachment video').css({
-      '-webkit-transform': 'translate(0, ' + vidOffsetY + 'px)',
-         '-moz-transform': 'translate(0, ' + vidOffsetY + 'px)',
-          '-ms-transform': 'translate(0, ' + vidOffsetY + 'px)',
-           '-o-transform': 'translate(0, ' + vidOffsetY + 'px)',
-              'transform': 'translate(0, ' + vidOffsetY + 'px)'
-    });
-    // if the video isn't about to be played, hide it
-    $('.video-attachment video').each(function(){
-      var vidPos = $(this).parent().position().top;
-      if ((currViewportPos - (winHeight)) > vidPos){
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-      if ((currViewportPos + (winHeight)) > vidPos){
-        $(this).show();
-      }
-    });
 
-    $('#cont video').each(function(){
-      var vidPos = $(this).position().top;
-      var vidHeight = $(this).height();
-      if (vidPos < currViewportPos || (vidPos > (currViewportPos + winHeight))){
-        $(this)[0].pause();
-        $(this).attr('muted', true);
-      } else {
-        $(this)[0].play();
-        $(this).attr('muted', false);
-      }
-    });
-
-    // hide miniMenu if it's on top of a gallery element
-    var gallPos = $('.thumb-gallery').position().top;
-    var gallHeight = $('.thumb-gallery').height();
-    if (((currViewportPos + 10) > gallPos) || (currViewportPos < (gallPos + gallHeight))){
-      $('#miniMenu').fadeOut();
-    } else {
-      $('#miniMenu').fadeIn();
+  $('#content h2').each(function(){
+    var secPoster = $(this).next();
+    if (secPoster.is('figure')){
+      var secPosterSrc = secPoster.find('img').attr('src');
+      $(this).attr('style','background: url("' + secPosterSrc + '") 50% 50% no-repeat transparent; background-size: cover');
+      $(this).css('line-height', winHeight + 'px');
+      // if (secPoster.next().is('.intro')){
+      //   var infoHeight = (secPoster.next().height() + 40);
+      //   secPoster.next().css({
+      //     'margin-top': '-' + (infoHeight / 3) + 'px'
+      //   });
+      // }
     }
-    console.log(currViewportPos + " " + parseInt(gallPos + gallHeight));
+    $(this).attr('data-offsety', $(this).position().top);
+  });
 
-  }, 1);
-
+  // deltaloop
+  setInterval(function(){
+    var currViewportPos = window.pageYOffset;
+    // $('h2').each(function(){
+      // if (currViewportPos > $(this).position().top){
+        // this is where the background image will do something
+      // }
+    // });
+  }, 25);
 
 });

b39f40d4976e3745ef459e86db05c312a9bbb0c1
Pete Haughie <speedwolf@gmail.com>
2014-03-31 23:17:45 +0100

    Set height to sections which have classes. Not sure if this is solid enough but it will hold for now.

--- static/js/storyteller.js
+++ static/js/storyteller.js
@@ -2,5 +2,14 @@ var winHeight = $(window).height();
 var winWidth = $(window).width();
 
 $(document).ready(function(){
+  // scope is private but can use global
+
+  $('section').each(function(){
+    if ($(this).attr('class')){
+      $(this).css({
+        'min-height': winHeight + 'px'
+      });
+    }
+  });
 
 });

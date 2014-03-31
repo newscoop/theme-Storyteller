f26c24fa1a2631c0324c5ffdc2f190407340319d
Pete Haughie <speedwolf@gmail.com>
2014-03-31 18:22:53 +0100

    Brief example of how jQ loads functions and in what order.

--- static/js/storyteller.js
+++ static/js/storyteller.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  
+  //  do this immediately
+  console.log('document loaded');
+});
+
+$(window).load(function(){
+  //  do this afterwards
+  console.log('window loaded');
 });

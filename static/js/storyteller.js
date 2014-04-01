78cdf34a15b86819dd7878024242ec8744d152df
Pete Haughie <speedwolf@gmail.com>
2014-04-01 12:59:39 +0100

    Cleaning up a little of the first part of this task

--- static/js/storyteller.js
+++ static/js/storyteller.js
@@ -3,11 +3,12 @@ var winWidth = $(window).width();
 
 $(document).ready(function(){
   // scope is private but can use global
+  console.log(winHeight - 60);
 
   $('section').each(function(){
     if ($(this).attr('class')){
       $(this).css({
-        'min-height': winHeight + 'px'
+        'min-height': (winHeight - 60) + 'px'
       });
     }
   });

322a94032a0c981b8f53eed4c04907a6560601ad
Pete Haughie <speedwolf@gmail.com>
2014-04-01 18:41:48 +0100

    Sexified the video playing behind mastheads.

--- static/js/storyteller.js
+++ static/js/storyteller.js
@@ -3,12 +3,16 @@ var winWidth = $(window).width();
 
 $(document).ready(function(){
   // scope is private but can use global
-  console.log(winHeight - 60);
 
   $('section').each(function(){
     if ($(this).attr('class')){
+      var paddingTop = ($(this).css('padding-top'));
+      paddingTop = paddingTop.replace('px','');
+      var paddingBottom = ($(this).css('padding-bottom'));
+      paddingBottom = paddingBottom.replace('px','');
+      var paddingVertical = parseInt(paddingTop) + parseInt(paddingBottom);
       $(this).css({
-        'min-height': (winHeight - 60) + 'px'
+        'min-height': (winHeight - paddingVertical) + 'px'
       });
     }
   });
--- static/modules/masthead_video.php
+++ static/modules/masthead_video.php
@@ -1,3 +1,8 @@
 <section class="masthead-video">
   <h2>Masthead with video</h2>
+  <video autoplay loop>
+    <source src="/video/video.mp4" type="video/mp4" />
+    <source src="/video/video.ogv" type="video/ogg" />
+    <source src="/video/video.webm" type="video/webm" />
+  </video>
 </section>

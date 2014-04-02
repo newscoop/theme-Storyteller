14c039ed6ef63c0586f60cbc1aad08a11309c97c
Pete Haughie <speedwolf@gmail.com>
2014-04-02 12:18:08 +0100

    More useful video object

--- static/modules/video.php
+++ static/modules/video.php
@@ -1,6 +1,8 @@
 <section class="video">
   <h2>Video</h2>
-  <video autoplay loop>
-    <source src="/video/video.mp4" />
+  <video controls>
+    <source src="/video/video.mp4" type="video/mp4" />
+    <source src="/video/video.ogv" type="video/ogg" />
+    <source src="/video/video.webm" type="video/webm" />
   </video>
 </section>

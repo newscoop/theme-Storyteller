4cccca4c57b3d63d58ebe2f258c5bc28db8a95de
Pete Haughie <speedwolf@gmail.com>
2014-04-02 19:59:28 +0100

    Added test text to the plain masthead

--- static/modules/masthead.php
+++ static/modules/masthead.php
@@ -1,3 +1,8 @@
 <section class="masthead">
   <h2>Masthead</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nunc malesuada, fermentum sem vel, facilisis eros. Sed in ligula quis felis bibendum eleifend. In sed hendrerit augue, hendrerit elementum odio. Pellentesque diam leo, varius auctor fermentum a, sollicitudin at magna. Curabitur molestie leo id interdum scelerisque. Curabitur enim mauris, fringilla et egestas cursus, imperdiet eget nibh. Suspendisse malesuada nibh ac dolor pharetra, a blandit arcu feugiat. Nunc pharetra malesuada metus, eget cursus augue.</p>
+  <blockquote>
+    <p>Sed tincidunt mi eu lectus euismod, non gravida ligula ullamcorper. Curabitur sodales, dolor at ultricies elementum, dui odio vehicula tellus, vel rutrum turpis enim id turpis. Vestibulum erat ante, dictum vitae pulvinar in, posuere nec est. Sed nec leo justo.</p>
+    <cite>Cicero, 45BC</cite>
+  </blockquote>
 </section>

b0dc342cde4b604bfe14293725052b614c7aadde
Pete Haughie <speedwolf@gmail.com>
2014-04-03 11:40:07 +0100

    Better type matching for null in the random string generator.

--- static/js/storyteller.js
+++ static/js/storyteller.js
@@ -19,8 +19,8 @@ $(document).ready(function(){
 
   // use this function to create IDs for objects that don't have them
   var randId = function(limit){
-    var randVal = ""; // we want a string the length of the input limit not an integer 
-    if (limit == null){
+    var randVal = ""; // we want a string the length of the input limit not an integer
+    if (limit === null || limit === undefined){
       limit = 4; // you can't have a random string of nothing so set default to 4 where there are 10000 (including 0000) possibilities
     }
     for (i = 0; i < limit; i = i + 1){

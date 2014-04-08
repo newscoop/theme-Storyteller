604a6f2d6c5caa4c25f2071c4ca4aa3ca04105a9
Pete Haughie <speedwolf@gmail.com>
2014-04-08 16:02:11 +0100

    Fixed image background now uses a class so any other image is unaffected by the show/hide js

--- static/modules/text_fixed_image.php
+++ static/modules/text_fixed_image.php
@@ -1,7 +1,7 @@
 <section class="image full">
   <h2>Fixed image and text</h2>
   <figure>
-    <img src="img/figure-text.jpg" />
+    <img src="img/figure-text.jpg" class="lead-image" />
     <figcaption>
       <h3>Mauris sed sem volutpat</h3>
       <p>In vel ante at <a href="#">nulla gravida</a> tincidunt a a mauris. <strong>Maecenas tempus</strong>, sapien id euismod aliquet, enim justo porta quam, a commodo dolor nulla quis urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut magna massa, sollicitudin in nulla non, ultrices ornare neque. Praesent vel ligula in quam pretium scelerisque. Pellentesque laoreet dui et neque porta laoreet. Quisque venenatis mauris enim, in molestie ante rhoncus quis. Phasellus aliquet ligula ac sem dictum dignissim. Vestibulum non tempus nulla. Sed ultricies imperdiet lacus vitae cursus.</p>

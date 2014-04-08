2765b6a53b04505c85cd2527e34323890e71f2d2
Pete Haughie <speedwolf@gmail.com>
2014-04-08 02:02:13 +0100

    More specific video and image masthead styles and markup

--- static/modules/masthead_image.php
+++ static/modules/masthead_image.php
@@ -1,3 +1,8 @@
 <section class="masthead-image">
   <h2>Masthead with image</h2>
+  <figure class="lead-image">
+    <img src="/img/figure.jpg" />
+  </figure>
+  <h3>Sub heading</h3>
+  <p>Aenean molestie nunc eros, in volutpat nibh vehicula vel. Aenean tempor, ligula ac interdum vulputate, odio mi scelerisque neque, id laoreet sapien tellus non turpis. Sed tincidunt mattis purus et varius. Morbi aliquet augue lorem, nec interdum nibh rhoncus semper. Vestibulum eu justo mollis, vulputate nulla ac, vestibulum neque. Vivamus lacus metus, consequat eget semper ac, accumsan at neque. Sed a est eget magna ullamcorper scelerisque quis eu elit. Sed laoreet erat interdum leo eleifend tincidunt id ac nisi. Phasellus egestas turpis vitae ligula accumsan, nec semper est laoreet.</p>
 </section>
--- static/modules/masthead_video.php
+++ static/modules/masthead_video.php
@@ -1,8 +1,10 @@
 <section class="masthead-video">
   <h2>Masthead with video</h2>
-  <video autoplay loop>
+  <video autoplay loop class="lead-video">
     <source src="/video/video.mp4" type="video/mp4" />
     <source src="/video/video.ogv" type="video/ogg" />
     <source src="/video/video.webm" type="video/webm" />
   </video>
+  <h3>Sub heading</h3>
+  <p>Aenean molestie nunc eros, in volutpat nibh vehicula vel. Aenean tempor, ligula ac interdum vulputate, odio mi scelerisque neque, id laoreet sapien tellus non turpis. Sed tincidunt mattis purus et varius. Morbi aliquet augue lorem, nec interdum nibh rhoncus semper. Vestibulum eu justo mollis, vulputate nulla ac, vestibulum neque. Vivamus lacus metus, consequat eget semper ac, accumsan at neque. Sed a est eget magna ullamcorper scelerisque quis eu elit. Sed laoreet erat interdum leo eleifend tincidunt id ac nisi. Phasellus egestas turpis vitae ligula accumsan, nec semper est laoreet.</p>
 </section>

fc7cc2a2d9bad42f2867f1c0fef8dd1235f7e2b5
Pete Haughie <speedwolf@gmail.com>
2014-04-08 14:20:20 +0100

    New video use case

---
+++ static/modules/text_fixed_video.php
@@ -0,0 +1,30 @@
+<section class="video full">
+
+  <h2>Fixed video and text</h2>
+
+  <video autoplay loop class="lead-video">
+    <source src="video/video.mp4" type="video/mp4" />
+    <source src="video/video.ogv" type="video/ogv" />
+    <source src="video/video.webm" type="video/webm" />
+  </video>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nunc malesuada, fermentum sem vel, facilisis eros. Sed in ligula quis felis bibendum eleifend. In sed hendrerit augue, hendrerit elementum odio. Pellentesque diam leo, varius auctor fermentum a, sollicitudin at magna. Curabitur molestie leo id interdum scelerisque. Curabitur enim mauris, fringilla et egestas cursus, imperdiet eget nibh. Suspendisse malesuada nibh ac dolor pharetra, a blandit arcu feugiat. Nunc pharetra malesuada metus, eget cursus augue.</p>
+
+  <h3>Sub heading</h3>
+
+  <blockquote>
+    <p>Sed tincidunt mi eu lectus euismod, non gravida ligula ullamcorper. Curabitur sodales, dolor at ultricies elementum, dui odio vehicula tellus, vel rutrum turpis enim id turpis. Vestibulum erat ante, dictum vitae pulvinar in, posuere nec est. Sed nec leo justo.</p>
+    <cite>Cicero, 45BC</cite>
+  </blockquote>
+
+  <figure>
+    <img src="img/figure-text.jpg" />
+    <figcaption>
+      <h3>Mauris sed sem volutpat</h3>
+      <p>In vel ante at <a href="#">nulla gravida</a> tincidunt a a mauris. <strong>Maecenas tempus</strong>, sapien id euismod aliquet, enim justo porta quam, a commodo dolor nulla quis urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut magna massa, sollicitudin in nulla non, ultrices ornare neque. Praesent vel ligula in quam pretium scelerisque. Pellentesque laoreet dui et neque porta laoreet. Quisque venenatis mauris enim, in molestie ante rhoncus quis. Phasellus aliquet ligula ac sem dictum dignissim. Vestibulum non tempus nulla. Sed ultricies imperdiet lacus vitae cursus.</p>
+    </figcaption>
+  </figure>
+
+  <p>Aenean molestie nunc eros, in volutpat nibh vehicula vel. Aenean tempor, ligula ac interdum vulputate, odio mi scelerisque neque, id laoreet sapien tellus non turpis. Sed tincidunt mattis purus et varius. Morbi aliquet augue lorem, nec interdum nibh rhoncus semper. Vestibulum eu justo mollis, vulputate nulla ac, vestibulum neque. Vivamus lacus metus, consequat eget semper ac, accumsan at neque. Sed a est eget magna ullamcorper scelerisque quis eu elit. Sed laoreet erat interdum leo eleifend tincidunt id ac nisi. Phasellus egestas turpis vitae ligula accumsan, nec semper est laoreet.</p>
+
+</section>

fff07c87b816a2e7d70eca71f48a6db417ffabe0
Pete Haughie <speedwolf@gmail.com>
2014-05-06 13:17:20 +0100

    More ambient audio files added to the fullscreen shutter slideshow to allow for testing

--- static/modules/slideshow_full_shutter.php
+++ static/modules/slideshow_full_shutter.php
@@ -45,7 +45,6 @@
     </li>
     <li>
       <figure>
-        <audio class="ambient" src="audio/audio.mp3"></audio>
         <img src="img/slideshow/shutter3.jpg" />
         <figcaption>
           <h3>Title 4</h3>
@@ -69,6 +68,7 @@
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
+      <audio class="ambient" src="audio/audio.mp3"></audio>
     </li>
     <li>
       <figure>
@@ -82,6 +82,7 @@
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
+      <audio class="ambient" src="audio/audio2.mp3"></audio>
     </li>
     <li>
       <figure>

9b13a55e7071ca2e073276aabcbefb6e5a894763
Pete Haughie <speedwolf@gmail.com>
2014-05-07 02:01:30 +0100

    Cleaning up the fullscreen slideshow

--- static/modules/slideshow_full_shutter.php
+++ static/modules/slideshow_full_shutter.php
@@ -7,7 +7,7 @@
         <figcaption>
           <h3>Title 1</h3>
           <dl>
-            <dt>Copyright:</dt>
+            <dt>Image Copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>

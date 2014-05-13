29f56f14859d902ffebd31fa6bdcb626d0c45f9c
Pete Haughie <speedwolf@gmail.com>
2014-05-13 19:39:22 +0100

    Fullscreen shutter slideshow now has fig caption elements in the right order.

--- static/modules/slideshow_full_shutter.php
+++ static/modules/slideshow_full_shutter.php
@@ -5,11 +5,11 @@
       <figure>
         <img src="img/slideshow/shutter1.jpg" />
         <figcaption>
-          <h3>Title 1</h3>
           <dl>
-            <dt>Image Image copyright:</dt>
+            <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 1</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
@@ -19,13 +19,12 @@
         <source src="video/video.mp4" type="video/mp4" />
         <source src="video/video.webm" type="video/webm" />
       </video>
-      <audio class="ambient" src="audio/audio.mp3"></audio>
       <span>
-        <h3>Title 2</h3>
         <dl>
-          <dt>Image copyright:</dt>
+          <dt>Video copyright:</dt>
             <dd>Pete Haughie</dd>
         </dl>
+        <h3>Title 2</h3>
         <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
       </span>
     </li>
@@ -33,11 +32,11 @@
       <figure>
         <img src="img/slideshow/shutter2.jpg" />
         <figcaption>
-          <h3>Title 3</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 3</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
           <p>Blah blah blah</p>
         </figcaption>
@@ -47,11 +46,11 @@
       <figure>
         <img src="img/slideshow/shutter3.jpg" />
         <figcaption>
-          <h3>Title 4</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 4</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
@@ -60,11 +59,11 @@
       <figure>
         <img src="img/slideshow/shutter4.jpg" />
         <figcaption>
-          <h3>Title 5</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 5</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
@@ -74,11 +73,11 @@
       <figure>
         <img src="img/slideshow/shutter5.jpg" />
         <figcaption>
-          <h3>Title 6</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 6</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
@@ -88,11 +87,11 @@
       <figure>
         <img src="img/slideshow/shutter6.jpg" />
         <figcaption>
-          <h3>Title 7</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 7</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
@@ -101,11 +100,11 @@
       <figure>
         <img src="img/slideshow/shutter7.jpg" />
         <figcaption>
-          <h3>Title 8</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 8</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
@@ -114,11 +113,11 @@
       <figure>
         <img src="img/slideshow/shutter8.jpg" />
         <figcaption>
-          <h3>Title 9</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 9</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
@@ -127,11 +126,11 @@
       <figure>
         <img src="img/slideshow/shutter9.jpg" />
         <figcaption>
-          <h3>Title 10</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 10</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>

6f1aee6e77a3954ae98dfc86d64e7534a5a1281b
Pete Haughie <speedwolf@gmail.com>
2014-05-14 08:58:29 +0100

    Added chapter descriptions

--- static/modules/chapter_title_video.php
+++ static/modules/chapter_title_video.php
@@ -1,6 +1,9 @@
 <section class="chapter-title">
   <article>
-    <h1>Chapter Title</h1>
+    <span class="title">
+      <h1>Chapter Title</h1>
+      <p>Description of chapter</p>
+    </span>
     <video autoplay loop>
       <source src="video/video.mp4" type="video/mp4" />
       <source src="video/video.webm" type="video/webm" />

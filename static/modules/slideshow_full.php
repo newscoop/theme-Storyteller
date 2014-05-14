c975800f8dcdb2cedd65a425a0521520b90fbde3
Pete Haughie <speedwolf@gmail.com>
2014-05-14 13:08:06 +0100

    Reusing existing images in fullscreen slideshows in the hope of making it clearer what's going on.

--- static/modules/slideshow_full.php
+++ static/modules/slideshow_full.php
@@ -2,59 +2,59 @@
   <ul class="slides">
     <li>
       <figure>
-        <img src="img/slideshow/img1.png" />
+        <img src="img/slideshow/shutter1.jpg" />
         <figcaption>
+          <dl>
+            <dt>Image copyright:</dt>
+              <dd>Pete Haughie</dd>
+          </dl>
           <h3>Title 1</h3>
-          <dl>
-            <dt>Image copyright:</dt>
-              <dd>Pete Haughie</dd>
-          </dl>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
     </li>
     <li>
       <figure>
-        <img src="img/slideshow/img2.png" />
+        <img src="img/slideshow/shutter2.jpg" />
         <figcaption>
+          <dl>
+            <dt>Image copyright:</dt>
+              <dd>Pete Haughie</dd>
+          </dl>
           <h3>Title 2</h3>
-          <dl>
-            <dt>Image copyright:</dt>
-              <dd>Pete Haughie</dd>
-          </dl>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
     </li>
     <li>
       <figure>
-        <img src="img/slideshow/img3.png" />
+        <img src="img/slideshow/shutter3.jpg" />
         <figcaption>
+          <dl>
+            <dt>Image copyright:</dt>
+              <dd>Pete Haughie</dd>
+          </dl>
           <h3>Title 3</h3>
-          <dl>
-            <dt>Image copyright:</dt>
-              <dd>Pete Haughie</dd>
-          </dl>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
     </li>
     <li>
       <figure>
-        <img src="img/slideshow/img4.png" />
+        <img src="img/slideshow/shutter4.jpg" />
         <figcaption>
-          <h3>Title 4</h3>
           <dl>
             <dt>Image copyright:</dt>
               <dd>Pete Haughie</dd>
           </dl>
+          <h3>Title 4</h3>
           <p>Sed magna arcu, euismod nec leo sit amet, viverra scelerisque odio. Cras vehicula, lorem eget lobortis vehicula, nisi lectus euismod ante, a malesuada augue nulla non quam. Maecenas felis est, posuere eget convallis eget, dictum eget libero. Integer accumsan elit lorem. Curabitur sapien augue, interdum eu velit vitae, lobortis lacinia felis. Nullam euismod mi in euismod vestibulum. Nulla facilisi. Praesent dictum volutpat odio, ac viverra nibh blandit non. Duis semper ac sapien id interdum. Aenean vel massa aliquam, commodo mauris at, pharetra elit. Nam consequat tortor magna, vitae rhoncus quam tincidunt sed. Vestibulum ac vestibulum magna. In ac tellus mauris. Ut eu venenatis orci. Sed quis lobortis justo, at volutpat metus. Proin nec semper dolor.</p>
         </figcaption>
       </figure>
     </li>
     <li>
       <figure>
-        <img src="img/slideshow/img5.png" />
+        <img src="img/slideshow/shutter5.jpg" />
         <figcaption>
           <h3>Title 5</h3>
           <dl>

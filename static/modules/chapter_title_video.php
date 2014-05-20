b474645fd4a7acb362e5cf7e4edf76fe0a5d812b
Pete Haughie <speedwolf@gmail.com>
2014-05-20 13:02:14 +0100

    Add poster image to chapter titles for auto background function

--- static/modules/chapter_title_video.php
+++ static/modules/chapter_title_video.php
@@ -4,7 +4,7 @@
       <h1>Chapter Title</h1>
       <p>Description of chapter</p>
     </span>
-    <video loop>
+    <video loop class="lead-video" poster="img/lead-video-poster.png">
       <source src="video/video.mp4" type="video/mp4" />
       <source src="video/video.webm" type="video/webm" />
     </video>

5406d6ee3ac566e20722a5f002d92784c2777ca5
Pete Haughie <speedwolf@gmail.com>
2014-06-17 15:53:20 +0100

    Unused random ID generator.

--- assets/js/storyteller/storyteller.js
+++ assets/js/storyteller/storyteller.js
@@ -10,19 +10,6 @@ var doWinDimensions = function(){
 
 $(document).ready(function(){
 
-  // use this function to create IDs for objects that don't have them - probably not an issue after template creation
-  var randId = function(limit){
-    var randVal = ""; // we want a string the length of the input limit not an integer
-    if (limit === null || limit === undefined){
-      limit = 4; // you can't have a random string of nothing so set default to 4 where there are 10000 (including 0000) possibilities
-    }
-    for (i = 0; i < limit; i = i + 1){
-      var randValPart = Math.floor(Math.random(0, limit) * 10);
-      randVal = randVal + randValPart;
-    }
-    return randVal;
-  };
-
   var getViewport = function(){
     var currViewport = window.pageYOffset;
     return currViewport;
